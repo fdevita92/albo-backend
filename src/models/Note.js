6a1f58a946d822085a78f05e2802e3a42dad90d7
Fabrizio De Vita <fdevita@unime.it>
2022-06-14 15:59:09 +0200

    Edited MongoDB schema

--- src/models/Note.js
+++ src/models/Note.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const noteSchema = new Schema({
-  text: { type: String, required: true },
+  //text: { type: String, required: true },
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  address: { type: String, required: true },
+  pec: { type: String, required: true },
+  taxcode: { type: String, required: true },
+  category: { type: String, required: true },
+  leaderboard:{type:String, required:true},
+  isWinner: { type: String, required: true },
+  winnerDate: { type: String, required: true },
 });
 
 const Note = mongoose.model('note', noteSchema);
