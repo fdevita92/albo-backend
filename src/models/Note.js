155b66b2478ce37234213f26b1a55e73e42cacab
Fabrizio De Vita <fdevita@unime.it>
2022-06-15 15:55:14 +0200

    Added "number" field to Note schema

--- src/models/Note.js
+++ src/models/Note.js
@@ -3,13 +3,14 @@ const { Schema } = mongoose;
 
 const noteSchema = new Schema({
   //text: { type: String, required: true },
+  number:  { type: Number, required: true },
   name: { type: String, required: true },
   location: { type: String, required: true },
   address: { type: String, required: true },
   pec: { type: String, required: true },
   taxcode: { type: String, required: true },
-  category: { type: String, required: true },
-  leaderboard:{type:String, required:true},
+  categories_soa: { type: Array, required: true },
+  categories_not_soa: { type: Array, required: true },
   isWinner: { type: String, required: false },
   winnerDate: { type: String, required: false },
 });
